dbd0be2c9f8dcdc8210a9a78ed3d075c3a4289b5
Eric <ericgdc94@gmail.com>
2017-03-24 12:50:44 +0100

    Cambios en archivos derivados de la migración
    Los cambios afectan a librerías, gradle, gitignore y otros archivos del
    proyecto que no son de código.

---
+++ desktop/src/com/icecubelab/elementcrash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.icecubelab.elementcrash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.icecubelab.elementcrash.vista.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
